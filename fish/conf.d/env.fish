fef3e1aeaa50b165fae2d15d455df1d8ea0040e3
Tosuke <tasukeprg@gmail.com>
2018-01-24 20:09:39 +0900

    :gear: add fish settings as a plugin

---
+++ fish/conf.d/env.fish
@@ -0,0 +1,3 @@
+# env
+set -x FZF_DEFAULT_OPTS '--height 40% --reverse --ansi --cycle'
+set -x EMOJI_CLI_FILTER 'fzf --height 15%'
---
+++ fish/functions/less.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 0
+function less --description 'alias less=less -R'
+	command less -R $argv;
+end
