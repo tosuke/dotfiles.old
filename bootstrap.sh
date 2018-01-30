c415d87ba9ec6afc912d3dd549f2c3d2b11c1670
Tosuke <tasukeprg@gmail.com>
2018-01-30 16:47:38 +0900

    :gear: install packages for archlinux

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,24 @@
+install-arch() {
+  echo "Install arch requirements"
+  
+  echo "> set makepkg.conf"
+  curl -Lo $HOME/.config/pacman/makepkg.conf --create-dirs 'https://raw.githubusercontent.com/Tosuke/dotfiles/master/arch/config/pacman/makepkg.conf'
+
+  echo "> install clang"
+  pacaur -S --noconfirm --needed clang
+  
+  echo "> install packages"
+  curl 'https://raw.githubusercontent.com/Tosuke/dotfiles/master/requirements-arch' \
+    | grep -ve '^$' -ve '^#' \
+    | xargs pacaur -S --noconfirm --noedit --needed
+}
+
+echo $1
 case $1 in
   arch)
+    install-arch
+  ;;
+  "")
   ;;
   *)
     echo "unsupported os" >&2
@@ -14,3 +33,4 @@ ghq get https://github.com/Tosuke/dotfiles.git
 
 cd $HOME/src/github.com/Tosuke/dotfiles
 fish deploy.fish $1
+source $HOME/.env.sh
