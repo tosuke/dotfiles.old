66d662f5f331ae856b325149a2948a94def12a85
Tosuke <tasukeprg@gmail.com>
2018-01-30 19:26:12 +0900

    :+1: daily-task

--- fish/theme/fish_greeting.fish
+++ fish/theme/fish_greeting.fish
@@ -1,2 +1,12 @@
 function fish_greeting
+  function __sync_vscode
+    echo 'Sync vscode extensions'
+    sync-vscode-extensions
+  end
+  function __sync_fisher
+    echo 'Sync fish plugin'
+    fisher up
+  end
+
+  daily-task '__sync_vscode' '__sync_fisher'
 end
