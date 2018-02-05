f588b92cf3660b8ba051a7533c5d63af6377e23f
Tosuke <tasukeprg@gmail.com>
2018-02-05 15:48:19 +0900

    :wrench: use term color

--- fish/theme/conf.d/color.fish
+++ fish/theme/conf.d/color.fish
@@ -1,22 +1,24 @@
 # Hybrid Next
-setu  fish_color_normal c5c8c6 # forground
-setu  fish_color_command 8abeb7 # brcyan
-setu  fish_color_quote b5bd68 # brgreen
-setu  fish_color_redirection 99dbff # brblue
-setu  fish_color_end 8abeb7 #brcyan
-setu  fish_color_error cc6666 # brred
-setu  fish_color_param c5c8c6 # foreground
-setu  fish_color_comment 6c7a80 # white
-setu  fish_color_match 242e33 --background=99bdff
-setu  fish_color_search_match $fish_color_normal --background=354751
-setu  fish_color_operator 8abeb7
-setu  fish_color_escape de935f --bold
-setu  fish_color_autosuggestion $fish_color_comment
-setu  fish_color_valid_path $fish_color_normal --underline
-setu  fish_color_history_current 99dbff --bold
-setu  fish_color_selection $fish_color_normal --background=354751
-setu  fish_color_cwd b5bd68
-setu  fish_pager_color_prefix de935f
-setu  fish_pager_color_completion $fish_color_normal --backfround=354751
-setu  fish_pager_color_description 8c9440
-setu  fish_pager_color_progress 8c9440
+set -l selection 354751
+
+setu fish_color_normal brwhite
+setu fish_color_command brcyan
+setu fish_color_quote brgreen
+setu fish_color_redirection brblue
+setu fish_color_end brcyan
+setu fish_color_error brred
+setu fish_color_param brwhite
+setu fish_color_comment white --italics
+setu fish_color_match black --background=brblue
+setu fish_color_search_match brwhite --background=$selection
+setu fish_color_operator brcyan
+setu fish_color_escape yellow --bold
+setu fish_color_autosuggestion white
+setu fish_color_valid_path $fish_color_normal --underline
+setu fish_color_history_current brblue --bold
+setu fish_color_selection $fish_color_normal --background=$selection
+setu fish_color_cwd brgreen
+setu fish_pager_color_prefix yellow
+setu fish_pager_color_completion $fish_color_normal --backfround=$selection
+setu fish_pager_color_description green
+setu fish_pager_color_progress green
