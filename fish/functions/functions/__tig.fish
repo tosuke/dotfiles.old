c7799cb10431b91923b57fbfbfac1d1717045a89
Tosuke <tasukeprg@gmail.com>
2018-02-05 15:12:39 +0900

    :gear: tig keybinds

---
+++ fish/functions/functions/__tig.fish
@@ -0,0 +1,4 @@
+function __tig
+  tig
+  commandline -f repaint
+end
--- fish/functions/key_bindings.fish
+++ fish/functions/key_bindings.fish
@@ -3,6 +3,7 @@ bind \ce 'end-of-line'
 bind \cr '__history_search_and_complete'
 bind \cx '__history_search_and_execute'
 bind \cg '__ghq_look'
+bind \ct '__tig'
 
 if bind -M insert >/dev/null ^/dev/null
   bind -M insert \ca 'beginning-of-line'
@@ -10,4 +11,5 @@ if bind -M insert >/dev/null ^/dev/null
   bind -M insert \cr '__history_search_and_complete'
   bind -M insert \cx '__history_search_and_execute'
   bind -M insert \cg '__ghq_look'
+  bind -M insert \ct '__tig'
 end
