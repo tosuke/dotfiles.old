c53698883f27a43e303e6a29e422bf2a79535008
Tosuke <tasukeprg@gmail.com>
2018-02-05 16:14:25 +0900

    :wrench: fix typo

--- fish/functions/functions/__history_search_and_execute.fish
+++ fish/functions/functions/__history_search_and_execute.fish
@@ -1,7 +1,7 @@
 function __history_search_and_execute
   __history_search (commandline -b) | read -lz select
   if not test -z $select
-    echo \n"execting: $select"
+    echo \n"executing: $select"
     eval $select
   end
   commandline -f repaint
