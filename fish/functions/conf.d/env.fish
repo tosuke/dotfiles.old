e88a70e21c232dcb4e00333b9613fb05c3883b03
Tosuke <tasukeprg@gmail.com>
2018-02-05 14:52:02 +0900

    :gear: enable emoji of yarn

--- fish/functions/conf.d/env.fish
+++ fish/functions/conf.d/env.fish
@@ -1,3 +1,4 @@
 # env
 set -x FZF_DEFAULT_OPTS '--height 40% --reverse --ansi --cycle'
 set -x EMOJI_CLI_FILTER 'fzf --height 15%' 'fzy' 'peco' 'percol'
+set -x YARN_DEFAULT_OPTS '--emoji true'
--- fish/functions/functions/yarn.fish
+++ fish/functions/functions/yarn.fish
@@ -1,4 +1,10 @@
 # Defined in - @ line 0
-function yarn --description 'alias yarn yarn --emoji true'
-	command yarn --emoji true $argv;
+function yarn 
+	set -l yarn_commands \
+		add autoclean bin cache check config create dedupe generate-lock-entry global help import info init install licenses link lockfile log{in,out} list \
+		outdated owner pack prune publish remove self-update tag team test unlink upgrade{,-interactive} version{,s} why
+
+	set -l cmd "command yarn $YARN_DEFAULT_OPTS $argv"
+	
+	eval $cmd
 end
