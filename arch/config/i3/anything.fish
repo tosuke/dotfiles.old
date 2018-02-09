85fd9359f728f69394d0ba197845f14de479f4f0
Tosuke <tasukeprg@gmail.com>
2018-02-09 18:06:37 +0900

    :wrench: update config

--- arch/config/i3/anything.fish
+++ arch/config/i3/anything.fish
@@ -1,1 +1,6 @@
-rofi -modi "combi,:$XDG_CONFIG_HOME/i3/system.fish,ssh,run" -combi-modi 'window,drun' -show combi -no-levenshtein-sort -sidebar-mode
+rofi -modi "combi,:$XDG_CONFIG_HOME/i3/system.fish,ssh,run"\
+  -combi-modi 'window,drun' \
+  -show combi \
+  -no-levenshtein-sort \
+  -sidebar-mode \
+  -window-match-fields 'class'
